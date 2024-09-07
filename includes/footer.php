e2704ec4b2a5465eb4c9395faef921c727755906
rlsolitario <solitarioranz@gmail.com>
2024-09-07 17:31:16 +0800

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved - DNB</b>
+      </div>
+      
+    </div>
+</footer>
